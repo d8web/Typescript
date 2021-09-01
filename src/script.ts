42f24cd974f08802ace7ece3c71fa0cd1feff5af
Daniel Ferreira <df46636@gmail.com>
2021-09-01 16:07:36 -0300

    Mais funcionalidades com Typescript

--- src/script.ts
+++ src/script.ts
@@ -4,7 +4,7 @@ const botao = document.getElementById('calcular') as HTMLButtonElement;
 const res = document.getElementById('resultado') as HTMLDivElement;
 
 function calcular(n1: number, n2:number) {
-    return n1 + n2
+    return n1 + n2;
 }
 
 botao.addEventListener('click', function() {
@@ -25,7 +25,7 @@ let programador: boolean = true;
 let animals: any = ['Girafa', 'Elefante', 'Joaninha', 'Formiga', 'Camaleão'];
 // Array de Números
 let ids: number[] = [12677, 26776, 35545, 53433, 56687];
-animals.push(200)
+animals.push(200);
 
 // Types function params
 function firstLetterUpperCase(name: string) {
@@ -34,3 +34,64 @@ function firstLetterUpperCase(name: string) {
 }
 
 firstLetterUpperCase('daniel'); // return Daniel
+
+// Types para objetos
+// Ponto de interrogação define a propriedade como opcional
+function resumo(usuario: {nome: string, idade?: number}) {
+    if(usuario.idade !== undefined) {
+        return `Olá ${usuario.nome}, tudo bem? Você tem ${usuario.idade} anos`;
+    } else {
+        return `Olá ${usuario.nome}, tudo bem?`;
+    }
+}
+
+let user = {
+    nome: 'Camila',
+}
+
+resumo(user);
+
+// Múltiplos types
+let cr7: string | number = 7;
+cr7 = '7';
+
+// Se a propriedade tiver múltiplos types, ao executar qualquer função ou coisa do tipo, precisa verificar o type da propriedade.
+// Exemplo abaixo:
+function mostrarAnimal(nome: string | number) {
+    if(typeof nome === 'string') {
+        console.log( nome.toUpperCase() );
+    } else {
+        console.log( nome );
+    }
+}
+
+mostrarAnimal('Cavalo');
+mostrarAnimal(45);
+
+// Type e interface
+type NameCompleto = string
+let myName: NameCompleto = 'Daniel';
+
+type NewIdade = string | number
+let myIdade: NewIdade = '25'
+
+/*
+type Car = {
+    name: string,
+    price: number
+}
+*/
+// Obs: Interface podemos adicionar novos types, com o type não podemos.
+interface Car {
+    name: string,
+    price: number
+}
+
+function mostrarCarro(carro: Car) {
+    return `O carro ${carro.name} custa R$ ${carro.price},00`
+}
+
+mostrarCarro({
+    name: 'Camaro',
+    price: 99000
+});
